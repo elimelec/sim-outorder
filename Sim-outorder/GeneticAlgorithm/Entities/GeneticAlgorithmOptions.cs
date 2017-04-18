a886c782a7fefef6817c380f1d8c0d37c8b3bddb
Eli <elimelec.burghelea@gmail.com>
2017-04-18 08:16:30 -0700

    Colapse { get; set; }

--- Sim-outorder/GeneticAlgorithm/Entities/GeneticAlgorithmOptions.cs
+++ Sim-outorder/GeneticAlgorithm/Entities/GeneticAlgorithmOptions.cs
@@ -5,45 +5,21 @@ namespace Simoutorder
 {
 	public class GeneticAlgorithmOptions
 	{
-		public int NumberOfCromozoms {
-			get;
-			set;
-		}
+		public int NumberOfCromozoms { get; set; }
 
-		public int NumberOfGenerations {
-			get;
-			set;
-		}
+		public int NumberOfGenerations { get; set; }
 
-		public int ElitesPercentage {
-			get;
-			set;
-		}
+		public int ElitesPercentage { get; set; }
 
-		public double CrossOverPercentage {
-			get;
-			set;
-		}
+		public double CrossOverPercentage { get; set; }
 
-		public double MutationOccurance {
-			get;
-			set;
-		}
+		public double MutationOccurance { get; set; }
 
-		public double MutationPercentage {
-			get;
-			set;
-		}
+		public double MutationPercentage { get; set; }
 
-		public List<string> Benchmarks {
-			get;
-			set;
-		}
+		public List<string> Benchmarks { get; set; }
 
-		public string SelectionMode {
-			get;
-			set;
-		}
+		public string SelectionMode { get; set; }
 	}
 }
 
