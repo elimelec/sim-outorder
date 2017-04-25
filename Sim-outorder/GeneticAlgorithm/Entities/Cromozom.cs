5f945d29121a070d7fa0c3b5a7c1f0705392b2f3
Eli <elimelec.burghelea@gmail.com>
2017-04-25 08:27:40 -0700

    Rename method

--- Sim-outorder/GeneticAlgorithm/Entities/Cromozom.cs
+++ Sim-outorder/GeneticAlgorithm/Entities/Cromozom.cs
@@ -31,31 +31,31 @@ namespace Simoutorder
 
 		public void Mutate()
 		{
-			MutateIf (() => { Configuration.CoreCkFreq = CromozomValues.CoreCkFreqValues.PickRandom();});
-			MutateIf (() => { Configuration.BusCkFreq = CromozomValues.BusCkFreq.PickRandom();});
-			MutateIf (() => { Configuration.lg2CacheSize = CromozomValues.lg2CacheSize.PickRandom();});
-			MutateIf (() => { Configuration.lg2Sets = CromozomValues.lg2Sets.PickRandom();});
-			MutateIf (() => { Configuration.lg2LineSize = CromozomValues.lg2LineSize.PickRandom();});
-			MutateIf (() => { Configuration.MissPenalty = CromozomValues.MissPenalty.PickRandom();});
-			MutateIf (() => { Configuration.WBPenalty = CromozomValues.WBPenalty.PickRandom();});
-			MutateIf (() => { Configuration.lg2StrSize = CromozomValues.lg2StrSize.PickRandom();});
-			MutateIf (() => { Configuration.lg2StrSets = CromozomValues.lg2StrSets.PickRandom();});
-			MutateIf (() => { Configuration.lg2StrLineSize = CromozomValues.lg2StrLineSize.PickRandom();});
-			MutateIf (() => { Configuration.StrMissPenalty = CromozomValues.StrMissPenalty.PickRandom();});
-			MutateIf (() => { Configuration.StrWBPenalty = CromozomValues.StrWBPenalty.PickRandom();});
-			MutateIf (() => { Configuration.lg2ICacheSize = CromozomValues.lg2ICacheSize.PickRandom();});
-			MutateIf (() => { Configuration.lg2ICacheSets = CromozomValues.lg2ICacheSets.PickRandom();});
-			MutateIf (() => { Configuration.lg2ICacheLineSize = CromozomValues.lg2ICacheLineSize.PickRandom();});
-			MutateIf (() => { Configuration.ICachePenalty = CromozomValues.ICachePenalty.PickRandom();});
-			MutateIf (() => { Configuration.NumCaches = CromozomValues.NumCaches.PickRandom();});
-			MutateIf (() => { Configuration.BranchStall = CromozomValues.BranchStall.PickRandom();});
-			MutateIf (() => { Configuration.StreamEnable = CromozomValues.StreamEnable;});
-			MutateIf (() => { Configuration.PrefetchEnable = CromozomValues.PrefetchEnable;});
-			MutateIf (() => { Configuration.LockEnable = CromozomValues.LockEnable;});
-			MutateIf (() => { Configuration.ProfGranularity = CromozomValues.ProfGranularity;});
+			Mutate (() => { Configuration.CoreCkFreq = CromozomValues.CoreCkFreqValues.PickRandom();});
+			Mutate (() => { Configuration.BusCkFreq = CromozomValues.BusCkFreq.PickRandom();});
+			Mutate (() => { Configuration.lg2CacheSize = CromozomValues.lg2CacheSize.PickRandom();});
+			Mutate (() => { Configuration.lg2Sets = CromozomValues.lg2Sets.PickRandom();});
+			Mutate (() => { Configuration.lg2LineSize = CromozomValues.lg2LineSize.PickRandom();});
+			Mutate (() => { Configuration.MissPenalty = CromozomValues.MissPenalty.PickRandom();});
+			Mutate (() => { Configuration.WBPenalty = CromozomValues.WBPenalty.PickRandom();});
+			Mutate (() => { Configuration.lg2StrSize = CromozomValues.lg2StrSize.PickRandom();});
+			Mutate (() => { Configuration.lg2StrSets = CromozomValues.lg2StrSets.PickRandom();});
+			Mutate (() => { Configuration.lg2StrLineSize = CromozomValues.lg2StrLineSize.PickRandom();});
+			Mutate (() => { Configuration.StrMissPenalty = CromozomValues.StrMissPenalty.PickRandom();});
+			Mutate (() => { Configuration.StrWBPenalty = CromozomValues.StrWBPenalty.PickRandom();});
+			Mutate (() => { Configuration.lg2ICacheSize = CromozomValues.lg2ICacheSize.PickRandom();});
+			Mutate (() => { Configuration.lg2ICacheSets = CromozomValues.lg2ICacheSets.PickRandom();});
+			Mutate (() => { Configuration.lg2ICacheLineSize = CromozomValues.lg2ICacheLineSize.PickRandom();});
+			Mutate (() => { Configuration.ICachePenalty = CromozomValues.ICachePenalty.PickRandom();});
+			Mutate (() => { Configuration.NumCaches = CromozomValues.NumCaches.PickRandom();});
+			Mutate (() => { Configuration.BranchStall = CromozomValues.BranchStall.PickRandom();});
+			Mutate (() => { Configuration.StreamEnable = CromozomValues.StreamEnable;});
+			Mutate (() => { Configuration.PrefetchEnable = CromozomValues.PrefetchEnable;});
+			Mutate (() => { Configuration.LockEnable = CromozomValues.LockEnable;});
+			Mutate (() => { Configuration.ProfGranularity = CromozomValues.ProfGranularity;});
 		}
 
-		private void MutateIf(Action action)
+		private void Mutate(Action action)
 		{
 			if (ShouldMutate()) {
 				action ();
