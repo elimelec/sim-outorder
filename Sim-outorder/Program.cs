1bfb4335e00fea4fa4282ef2332ddfa684cfe6c0
Eli <elimelec.burghelea@gmail.com>
2017-05-09 11:30:42 -0700

    Catch exceptions, write to console and show an alert

--- Sim-outorder/Program.cs
+++ Sim-outorder/Program.cs
@@ -7,10 +7,25 @@ namespace Simoutorder
 	{
 		public static void Main (string[] args)
 		{
+			GLib.ExceptionManager.UnhandledException += HandleException;
+
 			Application.Init ();
 			MainWindow win = new MainWindow ();
 			win.Show ();
 			Application.Run ();
 		}
+
+		public static void HandleException(GLib.UnhandledExceptionArgs args)
+		{
+			var exception = args.ExceptionObject as Exception;
+			Console.WriteLine (exception.StackTrace);
+			var message = exception.Message;
+			MessageDialog md = new MessageDialog (null, DialogFlags.DestroyWithParent, MessageType.Info, ButtonsType.Ok, message);
+			md.SetPosition (WindowPosition.Center);
+			md.Run ();
+			md.Destroy ();
+		}
 	}
+
+
 }
