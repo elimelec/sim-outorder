70bcb8e535f2e6aeae22e3c7a03a34688c344b96
Eli <elimelec.burghelea@gmail.com>
2017-05-09 13:09:02 -0700

    Extract NewRandom method in Configuration

--- Sim-outorder/GeneticAlgorithm/Entities/Configuration.cs
+++ Sim-outorder/GeneticAlgorithm/Entities/Configuration.cs
@@ -28,6 +28,38 @@ namespace Simoutorder
 		public bool PrefetchEnable;
 		public bool LockEnable;
 		public string ProfGranularity;
+
+		public static Configuration New()
+		{
+			var configuration = new Configuration ();
+
+			configuration.CoreCkFreq = CromozomValues.CoreCkFreqValues.PickRandom();
+			configuration.BusCkFreq = CromozomValues.BusCkFreq.PickRandom();
+			configuration.lg2CacheSize = CromozomValues.lg2CacheSize.PickRandom();
+			configuration.lg2Sets = CromozomValues.lg2Sets.PickRandom();
+			configuration.lg2LineSize = CromozomValues.lg2LineSize.PickRandom();
+			configuration.MissPenalty = CromozomValues.MissPenalty.PickRandom();
+			configuration.WBPenalty = CromozomValues.WBPenalty.PickRandom();
+			configuration.lg2StrSize = CromozomValues.lg2StrSize.PickRandom();
+			configuration.lg2StrSets = CromozomValues.lg2StrSets.PickRandom();
+			configuration.lg2StrLineSize = CromozomValues.lg2StrLineSize.PickRandom();
+			configuration.StrMissPenalty = CromozomValues.StrMissPenalty.PickRandom();
+			configuration.StrWBPenalty = CromozomValues.StrWBPenalty.PickRandom();
+			configuration.lg2ICacheSize = CromozomValues.lg2ICacheSize.PickRandom();
+			configuration.lg2ICacheSets = CromozomValues.lg2ICacheSets.PickRandom();
+			configuration.lg2ICacheLineSize = CromozomValues.lg2ICacheLineSize.PickRandom();
+			configuration.ICachePenalty = CromozomValues.ICachePenalty.PickRandom();
+			configuration.NumCaches = CromozomValues.NumCaches.PickRandom();
+			configuration.BranchStall = CromozomValues.BranchStall.PickRandom();
+			configuration.StreamEnable = CromozomValues.StreamEnable;
+			configuration.PrefetchEnable = CromozomValues.PrefetchEnable;
+			configuration.LockEnable = CromozomValues.LockEnable;
+			configuration.ProfGranularity = CromozomValues.ProfGranularity;
+			configuration.OptimizationLevel = CromozomValues.OptimizationLevel.PickRandom();
+			configuration.Memory = CromozomValues.Memory.PickRandom();
+
+			return configuration;
+		}
 	}
 }
 
